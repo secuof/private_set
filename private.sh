4d69b8b27ebd03d30bced16da80f2da5cb284628
David Hong <secuof@gmail.com>
2018-12-28 16:57:59 +0900

    modify private script, add line gitprompt

--- private.sh
+++ private.sh
@@ -1,5 +1,6 @@
 sudo apt-get update && sudo apt-get -y install vim curl git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 vim +PluginInstall +qall
